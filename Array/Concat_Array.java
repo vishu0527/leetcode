e82ec8e91b4a204b22fcbd6b5fe870d311b24258
vishu0527 <vspatil1627@gmail.com>
2025-11-16 23:15:54 +0530

    some changes

--- Array/Concat_Array.java
+++ Array/Concat_Array.java
@@ -3,9 +3,9 @@ package Array;
 class Solution {
     public int[] getConcatenation(int[] nums) {
         int n =nums.length;
-        int[] ans = new int[2 * n];
+        int[] ans = new int[2*n];
 
-        for(int i=0; i<n; i++) {
+        for(int i=0;i<n;i++) {
             ans[i]=nums[i];
             ans[i+n] =nums[i];
         }
